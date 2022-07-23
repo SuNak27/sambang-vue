7c18af42b8a851aa926ac678ed292d1c0037cf0c
Alfad Sabil Haq <alfadsabilhaq@gmail.com>
2022-07-23 08:09:26 +0700

    Informasi

--- src/router/index.js
+++ src/router/index.js
@@ -9,11 +9,11 @@ import Shift from "../views/Shift.vue";
 import Santri from "../views/Santri.vue";
 import Wilayah from "../views/Wilayah.vue";
 import Lembaga from "../views/Lembaga.vue";
+import Informasi from "../views/Informasi.vue";
 import Reservasi from "../views/Reservasi.vue";
 import TambahReservasi from "../views/TambahReservasi.vue";
 import DetailReservasi from "../views/DetailReservasi.vue";
 import History from "../views/History.vue";
-import Informasi from "../views/Informasi.vue";
 import Print from "../views/Print.vue";
 
 import axios from "axios";
@@ -23,7 +23,8 @@ import "@/assets/css/nprogress.css";
 // axios.defaults.headers.common["X-Sambang-Token"] =
 //   localStorage.getItem("token");
 
-axios.defaults.baseURL = "https://api-sambang.belanj.id/api/";
+// axios.defaults.baseURL = "https://api-sambang.belanj.id/api/";
+axios.defaults.baseURL = "http://localhost:3000/api/";
 NProgress.configure({ showSpinner: false });
 axios.interceptors.request.use((config) => {
   NProgress.start();
--- src/views/Informasi.vue
+++ src/views/Informasi.vue
@@ -1,213 +1,258 @@
 <template>
-  <div class="informasi">
+  <div class="home">
     <!--begin::Row-->
+    <CrudOnePage v-bind="crudOnePage" @modal="closeModal" />
+    <Crud
+      v-bind="{
+        multipleUrl: [{ url: '/informasi' }],
+      }"
+      @multipleResponse="multipleUrl"
+    />
     <div class="row g-5 g-xl-8">
-      <!--begin::Col-->
-      <div class="col-xl-12">
-        <!--begin::Tables Widget 1-->
-        <div class="card card-xl-stretch mb-xl-8">
-          <!--begin::Header-->
-          <div class="card-header border-0 pt-5">
-            <h3 class="card-title align-items-start flex-column">
-              <span class="card-label fw-bolder fs-3 mb-1"
-                >Pusat Informasi</span
+      <div class="card mb-5 mb-xl-8">
+        <div class="card-header">
+          <h3 class="card-title align-items-start flex-column">
+            <span class="card-label fw-bolder fs-3 mb-1">Pusat Informasi</span>
+          </h3>
+          <div class="card-toolbar">
+            <div
+              class="d-flex justify-content-end"
+              data-kt-user-table-toolbar="base"
+            >
+              <a
+                href="#"
+                class="btn btn-sm btn-success w-150px"
+                data-bs-toggle="modal"
+                data-bs-target="#tambah_informasi"
+                type="button"
+                @mouseover="crud(null, null, 'create')"
+                >Tambah Data</a
               >
-            </h3>
-            <div class="card-toolbar">
-              <div
-                class="d-flex justify-content-end"
-                data-kt-user-table-toolbar="base"
-              >
-                <a
-                  href="#"
-                  class="btn btn-sm btn-success w-150px"
-                  data-bs-toggle="modal"
-                  data-bs-target="#tambah_informasi"
-                  >Tambah Data</a
-                >
-
-                <TambahInformasi />
-              </div>
             </div>
-          </div>
-
-          <!--end::Header-->
-          <!--begin::Body-->
-          <div class="card-body py-3">
-            <!--begin::Table container-->
-            <div class="table-responsive">
-              <!--begin::Table-->
-              <table
-                id="informasi"
-                class="table text-center table-row-bordered gy-5"
-              >
-                <thead>
-                  <tr class="fw-bolder fs-6">
-                    <th class="w-50px text-center">No.</th>
-                    <th class="min-w-100px text-start">Nama Informasi</th>
-                    <th class="min-w-200px w-300px text-start">
-                      Detail Informasi
-                    </th>
-                    <th class="min-w-100px text-center">Tanggal Mulai</th>
-                    <th class="min-w-100px text-center">Tanggal Akhir</th>
-                    <th class="min-w-100px text-center">Status</th>
-                    <th class="min-w-100px text-center">Aksi</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="(info, no) in informasi.data" :key="info.id">
-                    <td>{{ ++no }}</td>
-                    <td class="text-start">{{ info.nama_informasi }}</td>
-                    <td class="text-start">{{ info.detail_informasi }}</td>
-                    <td>{{ info.tanggal_mulai }}</td>
-                    <td>{{ info.tanggal_akhir }}</td>
-                    <td
-                      :class="
-                        info.status == 'Buka' ? 'text-success' : 'text-danger'
-                      "
-                    >
-                      {{ info.status }}
-                    </td>
-                    <td class="d-flex justify-content-center">
-                      <a
-                        data-toggle="tooltip"
-                        data-bs-toggle="modal"
-                        :data-bs-target="'#edit_informasi_' + info.id"
-                        data-placement="top"
-                        title="Edit"
-                        class="
-                          btn
-                          btn-icon
-                          btn-bg-warning
-                          btn-active-text-primary
-                          btn-sm
-                          me-2
-                        "
-                      >
-                        <span class="bi bi-pencil" aria-hidden="true"></span
-                      ></a>
-                      <EditInformasi :res="info" />
-                      <a
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Buka"
-                        class="
-                          btn
-                          btn-icon
-                          btn-bg-success
-                          btn-active-text-primary
-                          btn-sm
-                          me-1
-                        "
-                        v-if="info.status == 'Nonaktif'"
-                        @click="clickAktif(--no, info.id)"
-                      >
-                        <span
-                          class="bi bi-power text-white"
-                          aria-hidden="true"
-                        ></span>
-                      </a>
-                      <a
-                        data-toggle="tooltip"
-                        data-placement="top"
-                        title="Tutup"
-                        class="
-                          btn
-                          btn-icon
-                          btn-bg-danger
-                          btn-active-text-primary
-                          btn-sm
-                          me-1
-                        "
-                        v-else
-                        @click="clickNonaktif(--no, info.id)"
-                      >
-                        <span
-                          class="bi bi-power text-white"
-                          aria-hidden="true"
-                        ></span>
-                      </a>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <!--end::Table-->
-            </div>
-            <!--end::Table container-->
           </div>
         </div>
-        <!--endW::Tables Widget 1-->
+        <div class="card-body py-3">
+          <div class="table-responsive">
+            <table
+              id="informasi_table"
+              class="table table-bordered dt-responsive nowrap"
+              style="width: 100%"
+            >
+              <thead>
+                <tr
+                  class="
+                    text-start text-muted
+                    fw-bolder
+                    fs-7
+                    text-uppercase
+                    gs-0
+                  "
+                >
+                  <th class="w-50px text-center">No</th>
+                  <th class="w-100px text-center">Nama Informasi</th>
+                  <th class="w-100px text-center">Tanggal Mulai</th>
+                  <th class="w-100px text-center">tanggal Selesai</th>
+                  <th class="w-100px text-center">Status</th>
+                  <th class="w-100px text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(informasi, no) in setInformasi.data"
+                  :key="informasi.id_informasi"
+                  class="text-center"
+                  :class="informasi.status == 'Nonaktif' ? 'text-danger' : ''"
+                >
+                  <td>{{ ++no }}</td>
+                  <td>{{ informasi.nama_informasi }}</td>
+                  <td>{{ informasi.tanggal_mulai }}</td>
+                  <td>{{ informasi.tanggal_akhir }}</td>
+                  <td>{{ informasi.status }}</td>
+                  <td class="text-center">
+                    <a
+                      data-toggle="tooltip"
+                      data-bs-toggle="modal"
+                      :data-bs-target="
+                        '#edit_informasi_' + informasi.id_informasi
+                      "
+                      data-placement="top"
+                      title="Edit"
+                      @mouseover="
+                        crud(
+                          informasi.id_informasi,
+                          informasi,
+                          'read',
+                          'View Data'
+                        )
+                      "
+                      href="#"
+                      class="
+                        btn btn-icon btn-bg-light btn-active-text-primary btn-sm
+                        me-1
+                      "
+                      ><span class="bi bi-eye" aria-hidden="true"></span
+                    ></a>
+                    <a
+                      data-toggle="tooltip"
+                      data-bs-toggle="modal"
+                      :data-bs-target="
+                        '#edit_informasi_' + informasi.id_informasi
+                      "
+                      data-placement="top"
+                      title="Edit"
+                      @mouseover="
+                        crud(
+                          informasi.id_informasi,
+                          informasi,
+                          'update',
+                          'Edit Data Informasi'
+                        )
+                      "
+                      href="#"
+                      class="
+                        btn btn-icon btn-bg-light btn-active-text-primary btn-sm
+                        me-1
+                      "
+                      ><span class="bi bi-pencil" aria-hidden="true"></span
+                    ></a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
-      <!--end::Col-->
     </div>
+    <!--end::Table container-->
+
     <!--end::Row-->
   </div>
 </template>
 
 <script>
-import TambahInformasi from "@/components/TambahInformasi.vue";
-import EditInformasi from "@/components/EditInformasi.vue";
-import axios from "axios";
+import CrudOnePage from "@/components/CrudOnePage.vue";
 import $ from "jquery";
-import Swal from "sweetalert2";
+import Crud from "@/components/Crud.vue";
 export default {
-  name: "Informasi",
-  components: { TambahInformasi, EditInformasi },
+  name: "Shift",
+  components: {
+    CrudOnePage,
+    Crud,
+  },
   data() {
     return {
       no: 1,
-      informasi: [],
+      setInformasi: [],
+      edit: 0,
+      crudOnePage: {},
+      crudField: [
+        {
+          type: "text",
+          title: "Nama Informasi",
+          name: "nama_informasi",
+          required: true,
+        },
+        {
+          type: "textarea",
+          title: "Detail Informasi",
+          name: "detail_informasi",
+          required: true,
+        },
+        {
+          type: "date",
+          title: "Tanggal Mulai",
+          name: "tanggal_mulai",
+          required: true,
+        },
+        {
+          type: "date",
+          title: "Tanggal Selesai",
+          name: "tanggal_akhir",
+          required: true,
+        },
+        {
+          type: "select",
+          title: "Status",
+          name: "status",
+          required: true,
+          data: [
+            {
+              text: "Buka",
+              value: "Buka",
+            },
+            {
+              text: "Tutup",
+              value: "Tutup",
+            },
+          ],
+          value: "",
+        },
+      ],
+      defaultField: [],
     };
   },
   methods: {
-    clickAktif(no, id) {
-      this.informasi.data[no].status = "Buka";
-      axios
-        .patch("/informasi/" + id, this.informasi.data[no])
-        .then()
-        .catch((error) => console.log(error));
-    },
-    clickNonaktif(no, id) {
-      this.informasi.data[no].status = "Tutup";
-      axios
-        .patch("/informasi/" + id, this.informasi.data[no])
-        .then()
-        .catch((error) => console.log(error));
-    },
-  },
-  mounted() {
-    axios
-      .get("/informasi")
-      .then((r) => {
-        this.informasi = r.data;
+    multipleUrl(value) {
+      if (value.url == "/informasi") {
+        this.setInformasi = value;
         $(document).ready(function () {
-          $("#informasi").DataTable({
-            responsive: false,
-            autoWidth: false,
+          $("#informasi_table").DataTable({
+            responsive: true,
             ordering: false,
           });
         });
-      })
-      .catch(function (error) {
-        if (error.response.status === 401) {
-          Swal.fire({
-            text: "Sesi telah berakhir, silahkan login kembali",
-            icon: "error",
-            buttonsStyling: !1,
-            confirmButtonText: "Ok",
-            customClass: {
-              confirmButton: "btn btn-primary order-2",
-            },
-          }).then((result) => {
-            if (result.isConfirmed) {
-              localStorage.removeItem("token");
-              window.location.href = "/login";
+      }
+    },
+    closeModal(r) {
+      if (r == false) {
+        for (let i = 0; i < this.crudField.length; i++) {
+          const element = this.crudField[i];
+          element.value = "";
+          delete element.selected;
+        }
+      }
+    },
+    crud(id, data, crud, title) {
+      if (crud == "create") {
+        for (let i = 0; i < this.crudField.length; i++) {
+          const element = this.crudField[i];
+          element.value = "";
+        }
+        this.crudOnePage = {
+          idmodal: "tambah_informasi",
+          crud: "create",
+          name: "Tambah Data Informasi",
+          fields: this.crudField,
+          url: "/informasi",
+          method: "post",
+          fieldDefaultAxios: this.defaultField,
+        };
+      } else {
+        for (let i = 0; i < this.crudField.length; i++) {
+          const element = this.crudField[i];
+          Object.keys(data).map(function (key) {
+            if (key == element.name) {
+              element.value = data[key];
             }
           });
+          if (element.type == "select") {
+            element.selected = element.data.find(
+              (option) => option.value == element.value
+            );
+          }
         }
-      });
+
+        this.crudOnePage = {
+          idmodal: "edit_informasi_" + id,
+          crud: crud,
+          name: title,
+          fields: this.crudField,
+          url: "/informasi/" + id,
+          method: "put",
+          fieldDefaultAxios: this.defaultField,
+        };
+      }
+    },
   },
 };
 </script>
-
-<style></style>
